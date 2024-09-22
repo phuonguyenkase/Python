a108e499dd0a815ca4d5db2d906caefb754410af
Kase <phuonguyen19042905@gmail.com>
2024-09-22 17:26:33 +0700

    paddle.py

---
+++ paddle.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.penup()
+        self.goto(position)
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid = 5, stretch_len = 1)
+
+    def up(self):
+        new_y = self.ycor() + 40
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 40
+        self.goto(self.xcor(), new_y)
